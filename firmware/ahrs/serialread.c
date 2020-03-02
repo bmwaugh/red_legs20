f56637b1e52d829f507e355b5d69adda6befff0f
Brandon Waugh <bmwaugh@wpi.edu>
2020-03-02 15:48:11 -0500

    added commits to serailread

--- firmware/ahrs/serialread.c
+++ firmware/ahrs/serialread.c
@@ -1,12 +1,19 @@
-#include <errno.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <termios.h>
-#include <unistd.h>
+#include <stdlib.h>  // Standard libray
+#include <stdio.h>   // Standard input/output definitions
+#include <string.h>  // String function definitions
+#include <unistd.h>  // UNIX standard function definitions
+#include <fcntl.h>   // File control definitions
+#include <errno.h>   // Error number definitions
+#include <termios.h> // POSIX terminal control definitions
+/*
+  Found sample code from:
+      https://www.cmrr.umn.edu/~strupp/serial.html
+*/
 
-int set_interface_attribs(int fd, int speed)
+/*
+  Sets up terminal interface to read the serial input correctly
+*/
+int set_interface_attribs(int fd, int speed) //fd is specified as an open port and speed is the baudrate
 {
     struct termios tty;
 
@@ -18,19 +25,19 @@ int set_interface_attribs(int fd, int speed)
     cfsetospeed(&tty, (speed_t)speed);
     cfsetispeed(&tty, (speed_t)speed);
 
-    tty.c_cflag |= (CLOCAL | CREAD);    /* ignore modem controls */
+    tty.c_cflag |= (CLOCAL | CREAD);    // ignore modem controls
     tty.c_cflag &= ~CSIZE;
-    tty.c_cflag |= CS8;         /* 8-bit characters */
-    tty.c_cflag &= ~PARENB;     /* no parity bit */
-    tty.c_cflag &= ~CSTOPB;     /* only need 1 stop bit */
-    tty.c_cflag &= ~CRTSCTS;    /* no hardware flowcontrol */
+    tty.c_cflag |= CS8;         // 8-bit characters
+    tty.c_cflag &= ~PARENB;     // no parity bit
+    tty.c_cflag &= ~CSTOPB;     // only need 1 stop bit
+    tty.c_cflag &= ~CRTSCTS;    // no hardware flowcontrol
 
-    /* setup for non-canonical mode */
+    // setup for non-canonical mode
     tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
     tty.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
     tty.c_oflag &= ~OPOST;
 
-    /* fetch bytes as they become available */
+    // fetch bytes as they become available
     tty.c_cc[VMIN] = 1;
     tty.c_cc[VTIME] = 1;
 
@@ -40,30 +47,16 @@ int set_interface_attribs(int fd, int speed)
     }
     return 0;
 }
-
-void set_mincount(int fd, int mcount)
-{
-    struct termios tty;
-
-    if (tcgetattr(fd, &tty) < 0) {
-        printf("Error tcgetattr: %s\n", strerror(errno));
-        return;
-    }
-
-    tty.c_cc[VMIN] = mcount ? 1 : 0;
-    tty.c_cc[VTIME] = 5;        /* half second timer */
-
-    if (tcsetattr(fd, TCSANOW, &tty) < 0)
-        printf("Error tcsetattr: %s\n", strerror(errno));
-}
-
+/*
+  converts output to ieee 754 floats
+    I am not sure if it is working properly yet.
+*/
 float hexToFloat(num) {
     //printf("0x%x\n",num);
     float f = *((float*)&num);
     printf("%f\n",f);
     return f;
 }
-
 
 int main()
 {
@@ -72,13 +65,14 @@ int main()
     int fd;
     int index;
     printf("B\n");
-    fd = open(portname, O_RDONLY | O_NOCTTY | O_SYNC | O_NONBLOCK);
+    fd = open(portname, O_RDONLY | O_NOCTTY | O_NONBLOCK);
+    //Read only, Not controlling terminal,  Nonblocking
     printf("C\n");
     if (fd < 0) {
         printf("Error opening %s: %s\n", portname, strerror(errno));
         return -1;
     }
-    /*baudrate 115200, 8 bits, no parity, 1 stop bit */
+    //baudrate 115200, 8 bits, no parity, 1 stop bit
     set_interface_attribs(fd, B115200);
 
     do {
@@ -86,17 +80,24 @@ int main()
         uint32_t xaccel, yaccel, zaccel, xgyro, ygyro, zgyro;
         int rdlen;
 
-        //rdlen = read(fd, buf, sizeof(buf) - 1);
         index = 0;
         while (index < sizeof(buf)) {
-          //printf("%d\n", sizeof(buf) - index);
           rdlen = read(fd, buf+index, sizeof(buf) - index);
-          //if (rdlen > 0)
             index += rdlen;
-          //printf("%d\n", index);
         }
 
+        //if read length is greater than 0, display the hex output
         if (rdlen > 0) {
+        // display hex
+            // unsigned char   *p;
+            // printf("\nRead %d:\n", rdlen);
+            // for (p = buf; index-- > 0; p++) {
+            //   printf(" 0x%x'", *p); //print hex
+            //   //printf("%c", *p); //print characters
+            // }
+            // printf("\n");
+
+        // if it reads all 90 bytes, then format into appropriate values
           if (rdlen == 90) {
             xaccel = (buf[6]<<24) | (buf[7]<<16)| (buf[8]<<8) | buf[9];
             yaccel = (buf[10]<<24) | (buf[11]<<16)| (buf[12]<<8) | buf[13];
@@ -104,30 +105,23 @@ int main()
             xgyro = (buf[19]<<24) | (buf[20]<<16)| (buf[21]<<8) | buf[22];
             ygyro = (buf[23]<<24) | (buf[24]<<16)| (buf[25]<<8) | buf[26];
             zgyro = (buf[27]<<24) | (buf[28]<<16)| (buf[29]<<8) | buf[30];
-
             printf("Xaccel:\n");
             hexToFloat(xaccel);
             printf("Yaccel:\n");
             hexToFloat(yaccel);
             printf("Zaccel:\n");
             hexToFloat(zaccel);
-        }
-
-        /* display hex */
-            unsigned char   *p;
-            printf("\nRead %d:\n", rdlen);
-            for (p = buf; index-- > 0; p++) {
-              //printf(" 0x%x'", *p); //print hex
-              //printf("%c", *p); //print characters
-            }
-            printf("\n");
-
-        /* make into good stuff*/
-
+            printf("Xgyro:\n");
+            hexToFloat(xgyro);
+            printf("Ygyro:\n");
+            hexToFloat(ygyro);
+            printf("Zgyro:\n");
+            hexToFloat(zgyro);
+          }
 
         } else if (rdlen == 0) {
             printf("Timeout from read\n");
         }
-        /* repeat read to get full message */
+        // repeat read to get full message
     } while (1);
 }
